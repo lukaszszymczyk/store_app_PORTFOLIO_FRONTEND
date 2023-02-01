7257ebafdd8300067708122f5d73838795b0c812
Łukasz Szymczyk <lukasz.szymczyk.94@gmail.com>
2023-02-01 23:33:44 +0100

    product-list: styles

--- src/components/atoms/ProductCard/index.tsx
+++ src/components/atoms/ProductCard/index.tsx
@@ -12,10 +12,10 @@ export function ProductCard({ product }: ProductCardProps): JSX.Element {
   return (
     <div className={styles.productCard}>
       <img className={styles.productCardImage} src={image} alt="product" />
-      <p>{title}</p>
-      <p>{price}</p>
-      <p>{description}</p>
-      <p>{category}</p>
+      <p className={styles.productCardTitle}>{title}</p>
+      <p className={styles.productCardItem}>{category}</p>
+      <p className={styles.productCardDescription}>{description}</p>
+      <p className={styles.productCardItem}>{price}</p>
     </div>
   );
 }
--- src/components/templates/HomeTemplate/index.tsx
+++ src/components/templates/HomeTemplate/index.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { Product } from "services/api/interfaces/Product";
 import { ProductList } from "components/organisms/ProductList";
+import styles from "./style.module.scss";
 
 export interface HomeTemplateProps {
   products: Product[];
 }
 
 export function HomeTemplate({ products }: HomeTemplateProps): JSX.Element {
-  return <ProductList products={products} />;
+  return (
+    <div className={styles.homeTemplate}>
+      <ProductList products={products} />
+    </div>
+  );
 }
