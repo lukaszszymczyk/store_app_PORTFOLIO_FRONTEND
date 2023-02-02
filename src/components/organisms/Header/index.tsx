7b8b14df14e01d161b4638af748b9920575769c6
Łukasz Szymczyk <lukasz.szymczyk.94@gmail.com>
2023-02-02 01:17:02 +0100

    product-details: styles

--- src/components/organisms/Header/index.tsx
+++ src/components/organisms/Header/index.tsx
@@ -3,16 +3,14 @@ import { Link } from "react-router-dom";
 import { CART_PAGE_PATH, HOME_PAGE_PATH } from "config/constants";
 import styles from "./style.module.scss";
 
-// https://reactrouter.com/en/main/components/link
-
 export function Header() {
   return (
     <div className={styles.header}>
-      <nav className={styles.links}>
-        <Link className={styles.headerLinks} to="/">
+      <nav className={styles.headerLinks}>
+        <Link className={styles.headerLinksElement} to={HOME_PAGE_PATH}>
           Home
         </Link>
-        <Link className={styles.element} to="/cart">
+        <Link className={styles.headerLinksElement} to={CART_PAGE_PATH}>
           Cart
         </Link>
       </nav>
--- src/components/templates/ProductTemplate/index.tsx
+++ src/components/templates/ProductTemplate/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Product } from "services/api/interfaces/Product";
 import { ProductDetails } from "components/organisms/ProductDetails";
+import styles from "./style.module.scss";
 
 export interface ProductTemplateProps {
   product: Product;
@@ -9,5 +10,9 @@ export interface ProductTemplateProps {
 export function ProductTemplate({
   product,
 }: ProductTemplateProps): JSX.Element {
-  return <ProductDetails product={product} />;
+  return (
+    <div className={styles.productTemplate}>
+      <ProductDetails product={product} />
+    </div>
+  );
 }
