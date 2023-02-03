c4619186f655944dbebbb9b53cc63d5c031c12b3
Łukasz Szymczyk <lukasz.szymczyk.94@gmail.com>
2023-02-03 16:53:28 +0100

    cart: useReducer

--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { HomeTemplate } from "components/templates/HomeTemplate";
 import { Spinner } from "components/atoms/Spinner";
-import { ErrorMessage } from "components/atoms/ErrorMessage";
+import { Message } from "components/molecules/Message";
 import { useProducts } from "./useProducts";
 
 export function Home(): JSX.Element {
   const { products, isLoading, errorMessage } = useProducts();
 
   if (!products) {
-    if (errorMessage) return <ErrorMessage message={errorMessage} />;
+    if (errorMessage) return <Message type="error" text={errorMessage} />;
     return <Spinner isLoading={isLoading} />;
   }
   return <HomeTemplate products={products} />;
