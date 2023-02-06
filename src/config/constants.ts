02536469390712025b4be737c06b63bc3927afbf
Łukasz Szymczyk <lukasz.szymczyk.94@gmail.com>
2023-02-06 10:22:01 +0100

    cart: payment

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -5,5 +5,5 @@ export const HOME_PAGE_PATH = "/";
 export const GET_PRODUCT_PAGE_PATH = (id: string) => `/products/${id}`;
 export const CART_PAGE_PATH = "/cart";
 export const CHECKOUT_PAGE_PATH = "/checkout";
-export const CHECKOUT_SUCCESS_PAGE_PATH = "/checkout/success";
-export const CHECKOUT_FAILURE_PAGE_PATH = "/checkout/failure";
+export const CHECKOUT_SUCCESS_PAGE_PATH = "/checkout/info?success=true";
+export const CHECKOUT_FAILURE_PAGE_PATH = "/checkout/info?success=false";
