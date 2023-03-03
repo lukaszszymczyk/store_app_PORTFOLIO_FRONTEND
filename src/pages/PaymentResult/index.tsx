9b09ceb9ad2e949059ad65350d8178e9a0e79cef
Łukasz Szymczyk <lukasz.szymczyk.94@gmail.com>
2023-03-03 08:58:21 +0100

    seo, favicon, toaster, fix: quantity select

--- src/pages/PaymentResult/index.tsx
+++ src/pages/PaymentResult/index.tsx
@@ -1,11 +1,24 @@
 import React from "react";
 import { useSearchParams } from "react-router-dom";
 import { Message } from "components/atoms/Message";
+import { PageWrapper } from "pages/PageWrapper";
 
 export function PaymentResult(): JSX.Element {
   const [searchParams] = useSearchParams();
   const isSuccess = searchParams.get("success") === "true";
 
-  if (!isSuccess) return <Message type="error" text="Failed payment" />;
-  return <Message type="success" text="Payment successful" />;
+  return (
+    <>
+      <PageWrapper
+        title="Store App payment result"
+        description="Result of payment"
+      >
+        {isSuccess ? (
+          <Message type="success" text="Payment successful" />
+        ) : (
+          <Message type="error" text="Failed payment" />
+        )}
+      </PageWrapper>
+    </>
+  );
 }
--- src/pages/Product/index.tsx
+++ src/pages/Product/index.tsx
@@ -1,17 +1,27 @@
 import React from "react";
 import { useFetchProduct } from "services/features/products/useFetchProduct";
 import { useParams } from "react-router";
+import { Spinner } from "components/atoms/Spinner";
 import { ProductTemplate } from "components/templates/ProductTemplate";
 import { Message } from "components/atoms/Message";
-import { Spinner } from "components/atoms/Spinner";
+import { PageWrapper } from "pages/PageWrapper";
 
 export function Product(): JSX.Element {
   const { id = "" } = useParams();
   const { product, isLoading, errorMessage } = useFetchProduct(id);
 
-  if (!product) {
-    if (errorMessage) return <Message type="error" text={errorMessage} />;
-    return <Spinner isLoading={isLoading} />;
-  }
-  return <ProductTemplate product={product} />;
+  return (
+    <>
+      <PageWrapper
+        title={product?.title || "Store App product details"}
+        description={`Store App: ${product?.title} details`}
+      >
+        {!product && errorMessage && (
+          <Message type="error" text={errorMessage} />
+        )}
+        {!product && !errorMessage && <Spinner isLoading={isLoading} />}
+        {product && <ProductTemplate product={product} />}
+      </PageWrapper>
+    </>
+  );
 }
